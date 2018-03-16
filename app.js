ecd4b719f8aea1ed30a8625f8367da0126a82196
German <magicnova@hotmail.com>
2018-03-16 20:28:49 +0100

    created user and message model

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { mongo } from 'mongoose';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,7 +10,9 @@ var bodyParser = require('body-parser');
 var appRoutes = require('./routes/app');
 
 var app = express();
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://gp:123456@ds161018.mlab.com:61018/ngnode');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -17,23 +21,22 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-    next();
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+	next();
 });
 
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    return res.render('index');
+app.use(function(req, res, next) {
+	return res.render('index');
 });
-
 
 module.exports = app;
---
+++ models/message.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+	content: {
+		type: String,
+		required: true
+	},
+	user: {
+		type: Schema.Types.ObjectId
+	}
+});
+
+module.exports = mongoose.model('Message', messageSchema);

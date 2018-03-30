9c48f03d0cc146be7e660eeca2c344644941738a
German <magicnova@hotmail.com>
2018-03-30 16:13:43 +0200

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-exports.SingUp = (req, res) => {
+exports.SignUp = (req, res) => {
 	const user = new User({
 		firstName: req.body.firstName,
 		lastName: req.body.lastName,
@@ -22,4 +23,42 @@ exports.SingUp = (req, res) => {
 			obj: result
 		});
 	});
+};
+
+exports.SignIn = (req, res) => {
+	User.findOne(
+		{ email: req.body.email },
+
+		function(err, user) {
+			if (err) {
+				return res.status(500).json({
+					title: 'An error ocurred',
+					error: err
+				});
+			}
+
+			if (!user) {
+				return res.status(500).json({
+					title: 'Login failed',
+					error: { message: 'Invalid credentials' }
+				});
+			}
+
+			if (!bcrypt.compareSync(req.body.password, user.password)) {
+				return res.status(401).json({
+					title: 'Login failed',
+					error: { message: 'Invalid credentials' }
+				});
+			}
+
+			const token = jwt.sign({ user: user }, 'secret', {
+				expiresIn: 7200
+			});
+			res.status(200).json({
+				message: 'Succesfully logged in',
+				token: token,
+				userId: user._id
+			});
+		}
+	);
 };
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/', userController.SingUp);
+router.post('/', userController.SignUp);
+router.post('/signin', userController.SignIn);
 
 module.exports = router;
